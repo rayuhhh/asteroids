ec1315c33a8bd8d9289c88303236f669c333703f
rayuhhh <rayuhhh@gmail.com>
2025-06-25 17:51:22 -0500

    created drawwables & updateable groups, so it can be done at once

--- main.py
+++ main.py
@@ -5,26 +5,37 @@ from circleshape import *
 
 def main():
     print("Starting Asteroids!")
-    print (f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
     clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
-    
+
+    dt = 0   
+
     while True:
         
-        screen.fill("black")
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             print(event.type)
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+
+        screen.fill("black")
+
+        for d in drawable:
+            d.draw(screen)
 
         pygame.display.flip()
+
+        # Frame limited to 60 fps
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
